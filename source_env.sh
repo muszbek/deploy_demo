9296d18531feea267c402c666a05409368b94fe8
tmuszbek <muszbektamas@gmail.com>
2023-02-27 15:25:58 +0100

    get aws data in a script for helmfile

---
+++ source_env.sh
@@ -0,0 +1,18 @@
+DOMAIN=example.com
+CLUSTER_NAME=demo-cluster
+
+AWS_REGION=$(aws configure get region)
+AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
+AWS_ACM_ARN=$(aws acm list-certificates --region $AWS_REGION --query "CertificateSummaryList[?DomainName==${DOMAIN}].CertificateArn" --output text)
+
+DOCKER_PW=$(aws ecr get-login-password --region $AWS_REGION)
+AWS_ECR_REPO=${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com
+docker login --username AWS --password $DOCKER_PW $AWS_ECR_REPO
+DOCKER_CREDS=$(cat ~/.docker/config.json)
+
+export DOMAIN=$DOMAIN
+export CLUSTER_NAME=$CLUSTER_NAME
+export AWS_ACCOUNT_ID=$AWS_ACCOUNT_ID
+export AWS_ACM_ARN=$AWS_ACM_ARN
+export AWS_ECR_REPO="${AWS_ECR_REPO}/"
+export DOCKER_CREDS=$DOCKER_CREDS
